2c69fb66aa098ddceb61feebe1d23bfbd3846c65
Malek Boubakri <malekbouba@gmail.com>
2019-04-28 11:49:32 +0100

    fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import CoursesPage from './pages/CoursesPage';
 
-const user = {
-  firstName: 'Malek',
-  lastName: 'Boubakri'
-};
-
-class App extends Component {
-  render() {
-    return (
+const App = () => (
       <Router>
-          <div>
+          <Fragment>
             <Route exact path="/" component={MainPage} />
             <Route exact path="/courses" component={CoursesPage} />
             <Route exact path="/login" component={LoginPage} />
-          </div>
+          </Fragment>
       </Router>
     );
-  }
-}
 
 export default App;
